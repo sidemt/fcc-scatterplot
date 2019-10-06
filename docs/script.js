0d989c0ae28f2719ac7852b63498970e5be7c029
sidemt <25644062+sidemt@users.noreply.github.com>
2019-10-06 21:23:21 +0900

    Create function to retrieve dataset from API

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,29 @@
-d3.select("body")
+const dataUrl = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+
+getDataset();
+
+// Retrieve the dataset
+function getDataset() {
+  // Instanciate XMLHttpRequest Object
+  req = new XMLHttpRequest();
+  // Initialize GET request
+  req.open("GET", dataUrl, true);
+  // Send the request
+  req.send();
+  // onload event handler
+  req.onload = function() {
+    // Parse the returned JSON string to JavaScript object
+    json = JSON.parse(req.responseText);
+    // use the value of "data" only
+    var dataset = json;
+    drawChart(dataset);
+  }
+}
+
+// Draw chart
+function drawChart(dataset) {
+  d3.select("#graph")
   .append("p")
-  .text("Hello world");
+  .text(JSON.stringify(dataset));
+}
+
