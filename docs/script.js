fc8e75496e9ccb153a0c4299047af56c0497d6fa
sidemt <25644062+sidemt@users.noreply.github.com>
2019-10-06 23:30:34 +0900

    Configure eslint

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,5 @@
-const dataUrl = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+const dataUrl =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 
 getDataset();
 
@@ -17,13 +18,54 @@ function getDataset() {
     // use the value of "data" only
     var dataset = json;
     drawChart(dataset);
-  }
-}
+  };
+};
 
 // Draw chart
 function drawChart(dataset) {
-  d3.select("#graph")
-  .append("p")
-  .text(JSON.stringify(dataset));
-}
+  // Width and height of the svg area
+  const w = 1000;
+  const h = 600;
+  const padding = 50;
 
+  // Style of dots
+  const r = 5;
+
+  // Min and max values
+  const minX = d3.min(dataset, (d) => d['Year'])
+  const maxX = d3.max(dataset, (d) => d['Year'])
+  const minY = d3.min(dataset, (d) => d['Seconds'])
+  const maxY = d3.max(dataset, (d) => d['Seconds'])
+
+  // Scales
+  // X: Year
+  const xScale = d3
+    .scaleTime()
+    .domain(
+      d3.extent(dataset, function(d) {
+        return new Date(d[0]);
+      })
+    )
+    .range([padding, w - padding]);
+
+  // Y: Time(Seconds)
+
+  const svg = d3
+    .select("#graph")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  svg
+    .selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => d['Year'])
+    .attr("cy", (d) => h - d['Seconds'])
+    .attr("r", r);
+
+  d3.select("#graph")
+    .append("p")
+    .text(JSON.stringify(dataset));
+};
